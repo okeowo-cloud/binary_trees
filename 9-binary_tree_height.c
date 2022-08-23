950be5eac9bd64093dccfc4c68a37c6834241e5c
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-08-23 00:10:02 -0700

    mandatory tasks

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t l_h, r_h;
 
-	if (!tree)
-		return (-1);
+	if (tree)
+	{
+		l_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return (l_h > r_h ? l_h : r_h);
+	}
 
-	left_height = binary_tree_height(tree->left) + 1;
-	right_height = binary_tree_height(tree->right) + 1;
-	return ((left_height > right_height) ? left_height : right_height);
+	return (0);
 }
