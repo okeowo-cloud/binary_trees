e2d1c64366bdb619c43602cb2fb0910ebe7ffe8c
junaideko <junaidekoraimot@gmail.com>
2022-08-24 04:03:27 -0700

    update node with at least one child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -16,5 +16,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	if ((!tree->left && tree->right) || (tree->left && !tree->right))
 		return (1);
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 }
