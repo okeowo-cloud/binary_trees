0cd7bb654f43c8d199665c0f418b4b76c4bb5f3b
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-08-24 11:31:49 -0700

    added 17-binary_tree_sibling.c

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: is a pointer to the node to find the sibling
+ * Return: a pointer to the sibling of the node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+
+	if (!node->parent)
+		return (NULL);
+
+	return (
+			node->parent->left == node
+			? node->parent->right :
+			node->parent->right == node
+			? node->parent->left : NULL
+		);
+}
