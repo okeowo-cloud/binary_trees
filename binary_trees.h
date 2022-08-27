f67ea213d5eea4c4aec3f381bfffdecf64c22fe4
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-08-26 17:02:38 -0700

    advanced

--- binary_trees.h
+++ binary_trees.h
@@ -30,6 +30,17 @@ typedef struct binary_tree_s avl_t;
 
 typedef struct binary_tree_s heap_t;
 
+/**
+ * struct levelorder_queue_s - Level order traversal queue.
+ * @node: A node of a binary tree.
+ * @next: The next node to traverse to in the binary tree.
+ */
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
+
 /* print a binary tree */
 void binary_tree_print(const binary_tree_t *);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
